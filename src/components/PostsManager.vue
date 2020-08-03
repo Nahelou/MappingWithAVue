d2bbde50fe548915d71075b935684f6757f7acf8
Quentin Nahélou <nahelouquentin@gmail.com>
2020-08-03 12:19:30 +0200

    Change basemap and add Geojson

--- src/components/PostsManager.vue
+++ src/components/PostsManager.vue
@@ -9,28 +9,58 @@
       @update:bounds="boundsUpdated"
     >
       <l-tile-layer :url="url"></l-tile-layer>
+      <l-geo-json :geojson="geojson"
+                :options="options"/>
     </l-map>
   </div>
 </template>
 
 <script>
 /* eslint-disable */
-import {LMap, LTileLayer} from 'vue2-leaflet';
+import {LMap, LTileLayer, LGeoJson, LMarker} from 'vue2-leaflet';
 
 export default {
   components: {
     LMap,
     LTileLayer,
+    LGeoJson,
+    LMarker
   },
   data () {
     return {
-      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      url: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
       zoom: 8,
       center: [47.413220, -1.219482],
-      bounds: null
+      bounds: null,
+      geojson: null
     };
   },
-  methods: {
+  computed: {
+    options() {
+      return {
+        onEachFeature: this.onEachFeatureFunction
+      };
+    },
+  onEachFeatureFunction() {
+      return (feature, layer) => {
+        layer.bindTooltip(
+          "<div>code:" +
+            feature.properties.code +
+            "</div><div>nom: " +
+            feature.properties.nom +
+            "</div>",
+          { permanent: false, sticky: true }
+        );
+      };
+  },
+  },
+  async created () {
+    const response = await fetch('https://rawgit.com/gregoiredavid/france-geojson/master/regions/bretagne/communes-bretagne.geojson');
+    const data = await response.json();
+    this.geojson = data;
+    this.loading = false;
+  },
+      methods: {
     zoomUpdated (zoom) {
       this.zoom = zoom;
     },
@@ -41,8 +71,11 @@ export default {
       this.bounds = bounds;
     }
   }
-}
+};
+
+
 </script>
+
 <style>
 
 </style>
